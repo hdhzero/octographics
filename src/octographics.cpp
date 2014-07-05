462d3c26f28b9571233036ce64e51afc4f165ff8
hdhzero <hdhzero@gmail.com>
2014-07-05 15:16:44 -0300

    fixed some bugs on perspective

--- src/octographics.cpp
+++ src/octographics.cpp
@@ -4,6 +4,9 @@
 #include <X11/Xlib.h>
 #include "/usr/include/X11/keysymdef.h"
 #include <unistd.h>
+#include <vector>
+#include <fstream>
+#include <sstream>
 #include "OctoGraphics.h"
 
 using namespace OctoGraphics;
@@ -11,58 +14,129 @@ using namespace std;
 
 XEvent event;
 bool k(char c) {
-//    std::cout << "tecla: " <<  XLookupKeysym(&event.xkey, 0) << std::endl;
-
     return  (XLookupKeysym(&event.xkey, 0) == c);
 }
 
-std::vector<OctoGraphics::Vertex> cube() {
+class Objs {
+    public:
+        std::vector<std::vector<OctoGraphics::Vertex> > faces;
+
+    public:
+        void read_obj(std::string filename) {
+            std::ifstream file;
+            std::string tmp;
+            std::vector<OctoGraphics::Vertex> vertices;
+            float v1, v2, v3;
+            int idx;
+
+            file.open(filename.c_str());
+
+            while (file.good()) {
+                getline(file, tmp);
+
+                if (tmp[0] != 'f' && tmp[0] != 'v') continue;
+                if (tmp[0] == 'v' && tmp[1] == 't') continue;
+
+                std::stringstream ss;
+                if (tmp[0] == 'v') {
+                    ss << tmp;
+                    ss >> tmp >> v1 >> v2 >> v3;
+                    vertices.push_back(Vertex(v1 / 1.0, v2 / 1.0, v3 / 1.0, 1.0));
+                } else {
+                    ss << tmp;
+                    ss >> tmp;
+                    faces.push_back(std::vector<OctoGraphics::Vertex>());
+
+                    while (ss >> idx) {
+                        faces[faces.size() - 1].push_back(vertices[idx - 1]);
+                    }
+                };
+            }
+        }
+
+        void draw_objs(OctoGraphics::Matrix& mt, OctoGraphics::Image& img) {
+            Vertex v1;
+            Vertex v2;
+            int sz;
+
+            for (int i = 0; i < faces.size(); ++i) {
+                sz = faces[i].size();
+
+                for (int j = 0; j <= faces[i].size(); ++j) {
+                    v1 = faces[i][j % sz];
+                    v2 = faces[i][(j + 1) % sz];
+                    img.draw_simple_line(mt.apply(v1), mt.apply(v2));
+                }
+            }
+        }
+};
+
+std::vector<OctoGraphics::Vertex> cube(float sz) {
     std::vector<OctoGraphics::Vertex> tmp;
     tmp.push_back(OctoGraphics::Vertex(0.0, 0.0, 0.0, 1.0));
-    tmp.push_back(OctoGraphics::Vertex(1.0, 0.0, 0.0, 1.0));
+    tmp.push_back(OctoGraphics::Vertex(sz, 0.0, 0.0, 1.0));
+
+    tmp.push_back(OctoGraphics::Vertex(0, 0, 0, 1.0));
+    tmp.push_back(OctoGraphics::Vertex(0, sz, 0, 1.0));
+
+    tmp.push_back(OctoGraphics::Vertex(sz, 0, 0, 1.0));
+    tmp.push_back(OctoGraphics::Vertex(sz, sz, 0, 1.0));
+
+    tmp.push_back(OctoGraphics::Vertex(0, sz, 0, 1));
+    tmp.push_back(OctoGraphics::Vertex(sz, sz, 0, 1));
 
     tmp.push_back(OctoGraphics::Vertex(0, 0, 0, 1));
-    tmp.push_back(OctoGraphics::Vertex(0, 1, 0, 1));
+    tmp.push_back(OctoGraphics::Vertex(0, 0, sz, 1));
 
-    tmp.push_back(OctoGraphics::Vertex(1, 0, 0, 1));
-    tmp.push_back(OctoGraphics::Vertex(1, 1, 0, 1));
+    tmp.push_back(OctoGraphics::Vertex(0, sz, 0, 1));
+    tmp.push_back(OctoGraphics::Vertex(0, sz, sz, 1));
 
-    tmp.push_back(OctoGraphics::Vertex(0, 1, 0, 1));
-    tmp.push_back(OctoGraphics::Vertex(1, 1, 0, 1));
+    tmp.push_back(OctoGraphics::Vertex(sz, sz, 0, 1));
+    tmp.push_back(OctoGraphics::Vertex(sz, sz, sz, 1));
 
-    tmp.push_back(OctoGraphics::Vertex(0, 0, 0, 1));
-    tmp.push_back(OctoGraphics::Vertex(0, 0, 1, 1));
+    tmp.push_back(OctoGraphics::Vertex(sz, 0, 0, 1));
+    tmp.push_back(OctoGraphics::Vertex(sz, 0, sz, 1));
 
-    tmp.push_back(OctoGraphics::Vertex(0, 1, 0, 1));
-    tmp.push_back(OctoGraphics::Vertex(0, 1, 1, 1));
+    tmp.push_back(OctoGraphics::Vertex(0, sz, sz, 1));
+    tmp.push_back(OctoGraphics::Vertex(sz, sz, sz, 1));
 
-    tmp.push_back(OctoGraphics::Vertex(1, 1, 0, 1));
-    tmp.push_back(OctoGraphics::Vertex(1, 1, 1, 1));
+    tmp.push_back(OctoGraphics::Vertex(0, 0, sz, 1));
+    tmp.push_back(OctoGraphics::Vertex(sz, 0, sz, 1));
 
-    tmp.push_back(OctoGraphics::Vertex(1, 0, 0, 1));
-    tmp.push_back(OctoGraphics::Vertex(1, 0, 1, 1));
+    tmp.push_back(OctoGraphics::Vertex(sz, sz, sz,1));
+    tmp.push_back(OctoGraphics::Vertex(sz, 0, sz, 1));
 
-    tmp.push_back(OctoGraphics::Vertex(0, 1, 1, 1));
-    tmp.push_back(OctoGraphics::Vertex(1, 1, 1, 1));
-
-    tmp.push_back(OctoGraphics::Vertex(0, 0, 1, 1));
-    tmp.push_back(OctoGraphics::Vertex(1, 0, 1, 1));
-
-    tmp.push_back(OctoGraphics::Vertex(1, 1, 1,1));
-    tmp.push_back(OctoGraphics::Vertex(1, 0, 1,1));
-
-    tmp.push_back(OctoGraphics::Vertex(0, 1, 1, 1));
-    tmp.push_back(OctoGraphics::Vertex(0, 0, 1, 1));
+    tmp.push_back(OctoGraphics::Vertex(0, sz, sz, 1));
+    tmp.push_back(OctoGraphics::Vertex(0, 0, sz, 1));
 
     return tmp;
 }
 
-void draw_cube(OctoGraphics::Matrix& mt, OctoGraphics::Image& img) {
-    std::vector<OctoGraphics::Vertex> cb = cube();
+void draw_cube(OctoGraphics::Matrix& mt, OctoGraphics::Image& img, float sz) {
+    std::vector<OctoGraphics::Vertex> cb = cube(sz);
 
     for (int q = 0; q < cb.size(); q += 2) {
         img.draw_simple_line(mt.apply(cb[q]), mt.apply(cb[q+1]));
     }
+}
+
+void draw_spin_cube(Image& img, float sz) {
+    Matrix m;
+    static float angle = 1.0f;
+    OctoGraphics::Vertex eye(0.0, 10.0, 10.0, 1.0);
+    OctoGraphics::Vertex gaze(0.0, 0.0, -100.0, 1.0);
+    OctoGraphics::Vertex view_up(0.0, 1.0, 0.0, 0.0);
+
+
+    m.identity();
+    m.rotate(Fixed(angle), Fixed(0.0), Fixed(0.0));
+    m.translate(Fixed(0.5f), Fixed(0.0f), Fixed(1.0f));
+    m.camera(eye, gaze, view_up);
+    m.perspective(Fixed(3.141592/4.0), (Fixed(640.0) / Fixed(480.0)), Fixed(-0.1), Fixed(-100.0));
+    m.viewport(640, 480);
+    
+    draw_cube(m, img, sz);
+    angle += 0.5f;
 }
 
 int main(int argc, char** argv) {
@@ -78,6 +152,11 @@ int main(int argc, char** argv) {
     OctoGraphics::Vertex gaze(0.0, 0.0, 0.0, 1);
     OctoGraphics::Vertex view_up(0.0, 1.0, 0.0, 0.0);
     OctoGraphics::Matrix m, mm;
+    Objs objs;
+    std::string filename(argv[1]);
+    objs.read_obj(filename);
+    //objs.read_obj("/home/hdhzero/untitled2.obj");
+    //objs.read_obj("/home/hdhzero/untitled.obj");
 
     Display *display;
     Window window;
@@ -132,7 +211,7 @@ int main(int argc, char** argv) {
             if (k('y')) { coord = 1; }
             if (k('z')) { coord = 2; }
             if (k('o')) { um = Fixed(1.0); }//major; }
-            if (k('p')) { um = Fixed(0.001); } //minor; }
+            if (k('p')) { um = Fixed(0.1); } //minor; }
             if (k('i')) { mfov += 1.0; }
             if (k('k')) { mfov -= 1.0; }
             if (k('q')) { break; }
@@ -225,10 +304,11 @@ int main(int argc, char** argv) {
 
             m.identity();
             m.camera(eye, gaze, view_up);
-//            m.perp(Fixed(-10.0), Fixed(-100.0));
+            //m.perp(Fixed(-10.0), Fixed(-100.0));
 //            m.orthographic(20.0, -20.0, 15.0, -15.0, -10, -100);
+            m.orthographic(16.0, -16.0, 12.0, -12.0, -10, -100);
             //m.orthographic(-20.0, 20.0, -15.0, 15.0, -10, -100);
-            m.perspective(Fixed(3.141592/mfov), (Fixed(640.0) / Fixed(480.0)), Fixed(-0.1), Fixed(-100.0));
+//            m.perspective(Fixed(3.141592/mfov), (Fixed(640.0) / Fixed(480.0)), Fixed(-10.1), Fixed(-20.0));
 
             color.set_rgb(0, 255, 255);
             img.set_color(color);
@@ -241,6 +321,8 @@ int main(int argc, char** argv) {
             m.print();
             X.print();
             m.apply(X).print();
+            m.apply(Y).print();
+            m.apply(Z).print();
             img.draw_simple_line(m.apply(X), m.apply(w));
             std::cout << "3vec\n";
 
@@ -256,14 +338,17 @@ int main(int argc, char** argv) {
 
             color.set_rgb(0, 0, 0);
             img.set_color(color);
-            draw_cube(m, img);
+            draw_cube(m, img, 1.1);
+            draw_spin_cube(img, 1.1);
+            objs.draw_objs(m, img);
 
             img.render_to_X(display, window, s);
             std::cout << "herez\n";
             std::cout << "eye: " << eye[0].to_float() << ' ' << eye[1].to_float() << ' ' << eye[2].to_float() << endl;
             std::cout << "gaze: " << gaze[0].to_float() << ' ' << gaze[1].to_float() << ' ' << gaze[2].to_float() << endl;
+            std::cout << "up: " << view_up[0].to_float() << ' ' << view_up[1].to_float() << ' ' << view_up[2].to_float() << endl;
 
-    //        XFlush(display);
+            XFlush(display);
 //            break;
         }
 
